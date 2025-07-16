df6e220a8a60fca99b5f9d8a11cbcffa6fc112ec
Nishant <nishantwebdev@outlook.in>
2025-07-16 22:42:04 +0530

    Setup Expressjs Server

---
+++ express/server.js
@@ -0,0 +1,15 @@
+// server.js
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
